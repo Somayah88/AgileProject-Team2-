59f60645b22afda30a713058d01af9fdf95e0147
Somayah88 <Somayah88@users.noreply.github.com>
2015-02-10 21:29:53 -0500

    Update GEDCOM.java

--- GEDCOM.java
+++ GEDCOM.java
@@ -2,35 +2,6 @@ package SOLUTION;
 
 
 import java.io.*;
-import java.util.Iterator;
-import java.util.LinkedList;
-/*class Info
-{
-
-
-   String indi, name, sex, birt, fams, famc;
-   String famid, ftag, IndiTag;
-   
-   public Info(String id, String n, String s, String b, String fs, String fc)
-   {
-	 indi=id; 
-     name = n; 
-     sex = s; 
-     birt = b; 
-     fams = fs; 
-     famc=fc;
-     //System.out.println( indi + " " + name + " "+ sex + " " + birt + " " + fams + " " + famc  );
-   }
-   
-   public Info(String famId, String fTag, String Inditag)
-   {
-	 famid = famId;
-	 ftag = fTag;
-	 IndiTag = Inditag;
-     //System.out.println( "Family Begin" + famId + " " + fTag +  " " + Inditag);
-   }
-}*/
-
 
 public class GEDCOM  {
 	
@@ -50,38 +21,23 @@ public class GEDCOM  {
             String Line;
             String name=null,sex = null,birt=null,famc=null,fams=null, id=null;
             String FamilyId=null, tag=null, IndiTag=null;
-	        int indiFlag=0, famFlag=0, item=0, item1=0;
+	        int indiFlag=0, item=0, item1=0;
 	        
             while (dis.available() != 0)
             {
             Line=dis.readLine();
 	       
 	        String[] info = Line.split(" ");
-            String Level = info[0];
+            //String Level = info[0];
             String Tag=null;
-            //System.out.println(Line);
-            //System.out.println("Level:"+Level);
             for(int l=0; l<info.length; l++)
             {
                 if(!(Character.isDigit(info[l].charAt(0))) && !(info[l].matches("^.*[^a-zA-Z0-9 ].*$")) )
                 {
                   Tag=info[l];
-                  //System.out.println("Tag Name   "+ info[l] + "\n");
                   break;
                 }
             }
-          /*   
-           
-           if (Tag.equals("INDI")|| (Tag.equals("NAME")) ||(Tag.equals("SEX"))||(Tag.equals("BIRT"))
-            		|| (Tag.equals("DEAT")) ||( Tag.equals("FAMC"))||( Tag.equals("FAMS")) ||(Tag.equals("FAM"))
-            		||( Tag.equals("MARR")) ||( Tag.equals("HUSB"))|| (Tag.equals("WIFE")) || (Tag.equals("CHIL"))|| 
-            				(Tag.equals("DIV") )|| (Tag.equals("DATE")) || (Tag.equals("TRLR"))|| (Tag.equals("NOTE")))
-            
-            System.out.println("Tag:"+Tag);
- 
-            else 
-            	System.out.println("Invalid Tag");
-          */  
             if (Tag.equals("NAME")) 
             {
             	name=info[2]+" "+info[3];
@@ -115,7 +71,6 @@ public class GEDCOM  {
             {
             	tag=info[1];
             	IndiTag=info[2];
-      		    //Info FamiliesData = new Info(FamilyId, tag, IndiTag);
       		    linkFamilies[item1] = FamilyId+" "+tag+" "+IndiTag;
                 item1++;
             }
@@ -123,7 +78,6 @@ public class GEDCOM  {
             {
             	tag=info[1];
             	IndiTag=info[2];
-            	//Info FamiliesData = new Info(FamilyId, tag, IndiTag);
             	linkFamilies[item1] = FamilyId+" "+tag+" "+IndiTag;
                 item1++;
             }
@@ -132,7 +86,6 @@ public class GEDCOM  {
             {
             	tag=info[1];
             	IndiTag=info[2];
-            	//Info FamiliesData = new Info(FamilyId, tag, IndiTag);
                 linkFamilies[item1] = FamilyId+" "+tag+" "+IndiTag;
                 item1++;
             }
@@ -142,7 +95,6 @@ public class GEDCOM  {
           if(Tag.equals("INDI") || Tag.equals("FAM"))
           {
         	  if( id!=null && indiFlag!=0){
-        		  //Info individualData = new Info(id, name,sex,birt,fams,famc);
         		  linkIndviduals[item] = id+" " +name+" " +sex+" " +birt+" " +fams+" " +famc;
         		  item++;
                   indiFlag = 0;
@@ -157,61 +109,41 @@ public class GEDCOM  {
           }
           
           if(Tag.equals("FAM")){
-        	  
-       	    /* if( FamilyId!= null){
-    		  Info FamiliesData = new Info(FamilyId, tag, IndiTag);
-              linkFamilies.add(FamiliesData);
-              famFlag = 0;
-              FamilyId = null;
-              tag=null;
-              IndiTag=null;
-             }*/
        	     FamilyId = info[1];
-        	 /*if(famFlag==1) {
-        		 famFlag=0;
-        		 item1=0;
-        	 }
-        	 famFlag++;*/
           }
         }
 	      fis.close();
 	      dis.close();
 	   	  
 	      printIndividuals();
+	      System.out.println("###########################");
 	      printFamilies();
 	      
 	  }
 	  
 	  private static void printIndividuals(){
-		  //String[] Names = new String[5000];
-	        for (int i=0; i<linkIndviduals.length; i++){
+	        for (int i=0; i<linkIndviduals.length && linkIndviduals[i] != null; i++){
 	          String[] info = linkIndviduals[i].split(" ");
-	          if(linkIndviduals[i] != null)
-	        	System.out.println(info[1] + " " + info[2]);
-	          else break;
+	          System.out.println(info[1] + " " + info[2]);
 	        }
 	  }
 	  
 	  private static void printFamilies(){
-		    //String[] Husbands = new String[1000]; 
-		    //String[] Wives = new String[1000]; 
-	        for (int i=0; i<linkFamilies.length; i++){
+	        for (int i=0; i<linkFamilies.length && linkFamilies[i] != null; i++){
 	          String[] info = linkFamilies[i].split(" ");
-	          System.out.println(linkFamilies[i]);
-	          if(linkFamilies[i] != null && info[1] == "HUSB"){
-	        	  for (int l=0; l<linkIndviduals.length; l++){
+	          if(info[1].equalsIgnoreCase("HUSB")){
+	        	  for (int l=0; l<linkIndviduals.length && linkIndviduals[l] != null; l++){
 	        		  String[] info1 = linkIndviduals[l].split(" ");
-	    	          if(info1[0] == info[3])
+	    	          if(info[2].equals(info1[0]) )
 	    	        	System.out.println("The husband Name:" + info1[1] + " " + info1[2]);
 	        	  }
-	          } else if(linkFamilies[i] != null && info[1] == "WIFE"){
-	        	  for (int l=0; l<linkIndviduals.length; l++){
+	          } else if(info[1].equalsIgnoreCase("WIFE") ){
+	        	  for (int l=0; l<linkIndviduals.length && linkIndviduals[l] != null; l++){
 	        		  String[] info1 = linkIndviduals[l].split(" ");
-	    	          if(info1[0] == info[3])
+	        		  if(info[2].equals(info1[0]) )
 	    	        	System.out.println("The wife Name:" + info1[1] + " " + info1[2]);
 	        	  }
 	          }
-	          else break;
 	        }
 	  }
 	  
